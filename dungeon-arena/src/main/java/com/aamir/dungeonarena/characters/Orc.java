1514f5ef01f4b1ef6c597b094f6255d91adc20a7
Aamir-shakil <aamirshakil21@gmail.com>
2026-04-09 14:01:51 +0100

    chore: fix project run configuration and compile output to target/classes

---
+++ dungeon-arena/src/main/java/com/aamir/dungeonarena/characters/Orc.java
@@ -0,0 +1,14 @@
+package com.aamir.dungeonarena.characters;
+
+/**
+ * A stronger enemy type with higher health and attack power.
+ */
+public class Orc extends Enemy {
+
+    /**
+     * Constructs an Orc with predefined stats.
+     */
+    public Orc() {
+        super("Orc", 80, 18);
+    }
+}
