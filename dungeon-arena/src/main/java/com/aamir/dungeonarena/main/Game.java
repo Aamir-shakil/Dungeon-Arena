4fdbbf6d5d8093f61d311a10aa1ad21012023ca1
Aamir-shakil <aamirshakil21@gmail.com>
2026-04-10 19:52:54 +0100

    feat: add round progression and multiple enemy encounters

--- dungeon-arena/src/main/java/com/aamir/dungeonarena/main/Game.java
+++ dungeon-arena/src/main/java/com/aamir/dungeonarena/main/Game.java
@@ -2,6 +2,7 @@ package com.aamir.dungeonarena.main;
 
 import com.aamir.dungeonarena.characters.Enemy;
 import com.aamir.dungeonarena.characters.Goblin;
+import com.aamir.dungeonarena.characters.Orc;
 import com.aamir.dungeonarena.characters.Player;
 import com.aamir.dungeonarena.state.GameState;
 import com.aamir.dungeonarena.state.MenuState;
@@ -15,18 +16,30 @@ public class Game {
     private Enemy currentEnemy;
     private GameState currentState;
     private boolean running;
+    private int round;
 
     /**
      * Starts the application.
      */
     public void start() {
         player = new Player("Knight", 100, 20);
-        currentEnemy = new Goblin();
+        round = 1;
+        currentEnemy = createEnemy(round);
         currentState = new MenuState();
         running = true;
 
         while (running) {
             currentState.handle(this);
+        }
+    }
+
+    public Enemy createEnemy(int round) {
+        if (round == 1) {
+            return new Goblin();
+        } else if (round == 2) {
+            return new Orc();
+        } else {
+            return new Orc(); // stronger later if we want
         }
     }
 
@@ -50,7 +63,17 @@ public class Game {
         this.currentState = currentState;
     }
 
+    public int getRound() {
+        return round;
+    }
+
+    public void nextRound() {
+        round++;
+        currentEnemy = createEnemy(round);
+    }
+
     public void stop() {
         running = false;
     }
+
 }
--- dungeon-arena/src/main/java/com/aamir/dungeonarena/state/VictoryState.java
+++ dungeon-arena/src/main/java/com/aamir/dungeonarena/state/VictoryState.java
@@ -2,16 +2,20 @@ package com.aamir.dungeonarena.state;
 
 import com.aamir.dungeonarena.main.Game;
 
-/**
- * Handles the victory state after a successful battle.
- */
 public class VictoryState implements GameState {
 
     @Override
     public void handle(Game game) {
         System.out.println();
-        System.out.println("You survived the battle!");
-        System.out.println("Thanks for playing.");
-        game.stop();
+        System.out.println("You won round " + game.getRound() + "!");
+
+        if (game.getRound() >= 3) {
+            System.out.println("You cleared the dungeon!");
+            game.stop();
+            return;
+        }
+
+        game.nextRound();
+        game.setState(new BattleState());
     }
 }
